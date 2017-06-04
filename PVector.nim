6cc9c0c64dd79cb02daa4cab22a06e906c8cf81b
Jacob <jacob.romano@workmail.com>
2017-06-04 13:29:39 +0200

    Fixed bug in PVector::dot()

--- PVector.nim
+++ PVector.nim
@@ -64,7 +64,7 @@ proc dist*(v1, v2: PVector): float =
 
 proc dot*(v1, v2: PVector): float =
   #TODO: look into Kahan summation
-  return v1.x*v2.x + v1.y*v2.y + v1.z+v2.z
+  return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
 
 proc cross*(v1, v2: PVector): PVector =
   var
