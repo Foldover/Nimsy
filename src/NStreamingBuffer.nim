8d5ddead407525cbab510a1348d4f5bb33156f9e
unknown <jacob.romano@workmail.com>
2017-08-21 20:21:33 +0200

    working on streaming buffer still

--- src/NStreamingBuffer.nim
+++ src/NStreamingBuffer.nim
@@ -4,7 +4,7 @@ import ../../nim-glfw/src/glad/gl
 
 const 
     STREAM_BUFFER_CAPACITY*: GLSizeiptr = 8192 * 1024 #8 megabytes
-    CHAR_BIT*: int = 8
+    CHAR_BIT*: int = 8 
 
 proc nextPowerOfTwo(number: var GLuint): GLuint =
     if number == 0:
@@ -25,6 +25,11 @@ type
         mStreamOffset: GLuint
         mDrawOffset: GLuint
 
+var
+    vertexData: seq[seq[float]]
+    lastIdx: int = 0
+    sb: seq[StreamedBuffer]
+
 proc newStreamedBuffer*() : StreamedBuffer =
     new(result)
     #generate buffers
@@ -37,34 +42,41 @@ proc newStreamedBuffer*() : StreamedBuffer =
         GL_STREAM_DRAW)
     glBindBuffer(GL_ARRAY_BUFFER, 0)
 
-proc uploadData*(sb: StreamedBuffer,  data: seq[float]) =
-    glBindBuffer(GL_ARRAY_BUFFER, sb.mVbo)
+proc queueVertices*(data: seq[float]) =
+    for n in lastIdx..lastIdx + data.len:
+        vertexData[0][n] = data[n - lastIdx]
+
+    lastIdx += data.len
+
+proc uploadData*(data: seq[float]) =
+    glBindBuffer(GL_ARRAY_BUFFER, sb[0].mVbo)
     var 
         dataSize = GLuint(data.sizeof)
         streamDataSize: GLuint = nextPowerOfTwo(dataSize)
     
-    if sb.mStreamOffset + streamDataSize > GLuint(STREAM_BUFFER_CAPACITY):
+    if sb[0].mStreamOffset + streamDataSize > GLuint(STREAM_BUFFER_CAPACITY):
         glBufferData(GL_ARRAY_BUFFER,
             STREAM_BUFFER_CAPACITY,
             nil,
             GL_STREAM_DRAW)
-        glBindVertexArray(sb.mVao)
-        glBindBuffer(GL_ARRAY_BUFFER, sb.mVbo)
+        glBindVertexArray(sb[0].mVao)
+        glBindBuffer(GL_ARRAY_BUFFER, sb[0].mVbo)
         glVertexAttribPointer(GLuint(0), GLint(3), cGL_FLOAT, GLboolean(0), GLsizei(0), cast[pointer](0))
         glBindVertexArray(0)
-        sb.mStreamOffset = 0
+        sb[0].mStreamOffset = 0
 
     var vertices = glMapBufferRange(GL_ARRAY_BUFFER,
-                        GLintptr(sb.mStreamOffset),
+                        GLintptr(sb[0].mStreamOffset),
                         GLsizeiptr(dataSize),
                         GL_MAP_WRITE_BIT or GL_MAP_UNSYNCHRONIZED_BIT)
 
     if vertices == nil:
         return
 
-    #upload data here somehow
+    for n in 0..lastIdx:
+        vertices[n] = vertexData[0][n]
 
     discard glUnmapBuffer(GL_ARRAY_BUFFER)
     glBindBuffer(GL_ARRAY_BUFFER, 0)
-    sb.mDrawOffset = sb.mStreamOffset div (GLfloat.sizeof * 3)
-    sb.mStreamOffset += dataSize
+    sb[0].mDrawOffset = sb[0].mStreamOffset div (GLfloat.sizeof * 3)
+    sb[0].mStreamOffset += dataSize
