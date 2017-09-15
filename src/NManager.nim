ea807a4dc6699463cfdc88bc889943dfae208773
Jacob <jacob.romano@workmail.com>
2017-09-15 19:58:19 +0200

    Some stuff I did long ago and can't remember

--- src/NManager.nim
+++ src/NManager.nim
@@ -1,18 +1,56 @@
 include NStreamingBuffer
+from Nimsytypes import DrawingModes
+
+import Nimsyglobals
+
+type
+    CommandQueue = ref object
+        commands*: seq[DrawingModes]
+        gldrawmode*: seq[GLenum]
+        timestamp*: seq[int]
+        count*: seq[int]
+
+proc newCommandQueue(): CommandQueue =
+    new(result)
+    newSeq(result.commands, 0)
+    newSeq(result.timestamp, 0)
+    newSeq(result.count, 0)
+    newSeq(result.gldrawmode, 0)
+
+type
+    RenderingObject = ref object
+        vertices: array[STREAM_BUFFER_CAPACITY_INT div GLfloat.sizeof, float]
+        lastIdx: int
+        streamedBuffer: StreamedBuffer
+        commands: CommandQueue
+
+proc newRenderingObject(): RenderingObject =
+    new(result)
+    result.streamedBuffer = newStreamedBuffer()
+    result.commands = newCommandQueue()
+    result.lastidx = 0
 
 type
     NManager = ref object
-        vertices: seq[seq[float]]
-        lastIdx: array[8, int]
-        streamedBuffers: seq[StreamedBuffer]
+        renderObj: RenderingObject
 
 proc newManager*(): NManager =
     new(result)
-    newSeq(result.streamedBuffers, 1)
-    result.streamedBuffers[0] = newStreamedBuffer()
+    result.renderObj = newRenderingObject()
 
-proc queueVertices*(man: NManager, data: seq[float]) =
+proc queueVertices*(man: NManager, data: seq[float], mode: DrawingModes, glmode: GLenum) =
+    man.renderObj.commands.commands.add(mode)
+    man.renderObj.commands.timestamp.add(lastIdx)
+    man.renderObj.commands.count.add(data.len)
+    man.renderObj.commands.gldrawmode.add(glmode)
     for n in lastIdx..lastIdx + data.len:
-        man.vertices[0][n] = data[n - man.lastIdx[0]]
+        man.renderObj.vertices[n] = data[n - man.renderObj.lastIdx]
 
-    man.lastIdx[0] += data.len
+    man.renderObj.lastIdx += data.len
+
+proc doRender(man: NManager) =
+    for n in 0..man.renderObj.commands.commands.high:
+        glBindVertexArray(man.renderObj.streamedBuffer.mVao)
+        glBindBuffer(GL_ARRAY_BUFFER, man.renderObj.streamedBuffer.mVbo)
+        glVertexAttrib1f(GLuint(drawingModeLocation), GLfloat(man.renderObj.commands.commands[n]))
+        glDrawArrays(man.renderObj.commands.gldrawmode[n], GLint(man.renderObj.commands.timestamp[n]), GLsizei(man.renderObj.commands.count[n]))
--- src/NStreamingBuffer.nim
+++ src/NStreamingBuffer.nim
@@ -4,6 +4,7 @@ import ../../nim-glfw/src/glad/gl
 
 const 
     STREAM_BUFFER_CAPACITY*: GLSizeiptr = 8192 * 1024 #8 megabytes
+    STREAM_BUFFER_CAPACITY_INT*: int = int(STREAM_BUFFER_CAPACITY)
     CHAR_BIT*: int = 8 
 
 proc nextPowerOfTwo(number: var GLuint): GLuint =
@@ -21,7 +22,6 @@ type
     StreamedBuffer* = ref object
         mVao: GLuint
         mVbo: GLuint
-        mVertices: seq[float]
         mStreamOffset: GLuint
         mDrawOffset: GLuint
 
@@ -41,36 +41,54 @@ proc newStreamedBuffer*() : StreamedBuffer =
     glBindBuffer(GL_ARRAY_BUFFER, 0)
 
 proc uploadData*(sb: StreamedBuffer, data: seq[float]) =
+    #bind the streambuffer's vbo
     glBindBuffer(GL_ARRAY_BUFFER, sb.mVbo)
+
+    #get the size of the data to upload, as well as the size of streamed buffer to use
     var 
         dataSize = GLuint(data.sizeof)
         streamDataSize: GLuint = nextPowerOfTwo(dataSize)
     
+    #check if the incumbent data upload will overshoot the capacity of the streamed buffer
     if sb.mStreamOffset + streamDataSize > GLuint(STREAM_BUFFER_CAPACITY):
+        #orfan the buffer
         glBufferData(GL_ARRAY_BUFFER,
             STREAM_BUFFER_CAPACITY,
             nil,
             GL_STREAM_DRAW)
+        #bind the vao
         glBindVertexArray(sb.mVao)
+        #bind the vbo
         glBindBuffer(GL_ARRAY_BUFFER, sb.mVbo)
+        #tell opengl how to interpret the data in the vbo
         glVertexAttribPointer(GLuint(0), GLint(3), cGL_FLOAT, GLboolean(0), GLsizei(0), cast[pointer](0))
+        #unbind vao
         glBindVertexArray(0)
+        #reset the stream offset counter
         sb.mStreamOffset = 0
 
+    #get a range "streamoff<=range<=datasize" for writing. Tell opengl that no synchronization is needed, because we
+    #can promise to not write to unfinished queued ranges of buffer commands, as well as promise to not read memory.
     var vertices = glMapBufferRange(GL_ARRAY_BUFFER,
                         GLintptr(sb.mStreamOffset),
                         GLsizeiptr(dataSize),
                         GL_MAP_WRITE_BIT or GL_MAP_UNSYNCHRONIZED_BIT)
 
+    #if we didn't get any memory range, just fail silently
     if vertices == nil:
         return
 
+    #cast the void* pointer to a float* pointer (needs testing)
     var verticesFloat = cast[ptr array[0 .. 0, float]](vertices)
 
+    #write to the mapped memory
     for n in 0..lastIdx:
-        verticesFloat[n] = sb.mVertices[n]
+        verticesFloat[n] = data[n]
 
+    #unmap tells opengl we're done
     discard glUnmapBuffer(GL_ARRAY_BUFFER)
+    #unbind vbo
     glBindBuffer(GL_ARRAY_BUFFER, 0)
+    #update draw and stream offset
     sb.mDrawOffset = sb.mStreamOffset div (GLfloat.sizeof * 3)
     sb.mStreamOffset += dataSize
